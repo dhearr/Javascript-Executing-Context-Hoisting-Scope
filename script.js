2e7eae6f61ad68725da1cb7332f7aa94361bbb46
dhearr <dhearr27@gmail.com>
2024-01-28 20:48:08 +0700

    first update

---
+++ script.js
@@ -0,0 +1,51 @@
+// =====================================================================================================
+// // EXECUTING CONTEXT, HOISTING, SCOPE
+// console.log(sayHallo());
+
+// var nama = "dhea ramdani";
+// var umur = 23;
+
+// function sayHallo() {
+//   return `Hallo nama saya ${nama}, Saya berumur ${umur} tahun.`;
+// }
+
+// var nama = "dhea ramdani";
+// var userName = "@dherrrrrrrrrrrr";
+
+// function cetakUrl(userName) {
+//   var instagramUrl = "https://instagram.com/";
+//   return instagramUrl + userName;
+// }
+
+// console.log(cetakUrl(userName));
+
+// function a() {
+//   console.log("ini a");
+
+//   function b() {
+//     console.log("ini b");
+
+//     function c() {
+//       console.log("ini c");
+//     }
+//     c();
+//   }
+//   b();
+// }
+// a();
+
+// function satu() {
+//   var nama = "Dhea";
+//   console.log(nama);
+// }
+
+// function dua() {
+//   console.log(nama);
+// }
+
+// console.log(nama);
+// var nama = "Afani";
+// satu();
+// dua("Akhira");
+// console.log(nama);
+// =====================================================================================================
